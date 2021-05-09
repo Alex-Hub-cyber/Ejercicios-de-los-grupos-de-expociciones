73327f2e8b23623bd825adfbb227f0652aac8ec4
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-05-09 17:08:02 -0500

    Grupo1.
    Grupo1

---
+++ EjemploGrupo1/EjemploGrupo1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploGrupo1
+{
+    public partial class FrmFormulario : Form
+    {
+        public FrmFormulario()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFormulario_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        
+        public void Insert(string nom)
+            {
+
+                List<string> Lista = new List<string>();
+
+                Lista.Add(nom);
+
+                foreach (string ver in Lista)
+                {
+                      DtgDatosAmostrar.Rows.Add(ver);
+                }
+
+                TxtDatos.Text = "";
+            }
+
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            Insert(TxtDatos.Text);
+        }
+    }
+    }
+
